cb2e3cc4dde67633cdcbb6752a55eeeba0405c0f
kalim-sahebi <kalimapid@yahoo.com>
2023-10-22 12:58:01 +0430

    Initial commit

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:expenses/widgets/expenses_list/expense_item.dart';
+import 'package:expenses/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
